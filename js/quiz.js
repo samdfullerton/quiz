80d9ee0b87359464337174901671e4403172fcb7
Sam Fullerton <Samdfullerton@gmail.com>
2020-09-30 18:52:33 -0500

    created startQuiz function to start the quiz

--- js/quiz.js
+++ js/quiz.js
@@ -8,7 +8,7 @@ var questions = [{
     answer:"black"
 },  {
     question:"How many feet are in a mile?",
-    choices:["525000", "60", "5280", "2300"]
+    choices:["525000", "60", "5280", "2300"],
     answer:"5280"
 }
 ]
@@ -16,4 +16,31 @@ var questions = [{
 var startDiv = document.getElementById("start");
 var questionsDiv = document.getElementById("questions")
 var endDiv = document.getElementById("end")
+var startBtn = document.getElementById("startBtn")
 var questionIndex = 0
+var timer = 60
+
+
+function startQuiz(){
+    startDiv.setAttribute("class", "hide");
+    questionsDiv.removeAttribute("class"); 
+    //code to start the timer here
+
+    renderQuestions()
+    
+}
+function renderQuestions(){
+    var currentQuestion = questions[questionIndex]
+
+}
+function answerClick(){
+
+}
+function gameOver(){
+
+}
+function saveHighScore(){
+
+}
+startBtn.onclick = startQuiz;
+
