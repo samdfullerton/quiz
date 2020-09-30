d4bb0655ca7a49db4a837df0c6f0cb9d55b1d88e
Sam Fullerton <Samdfullerton@gmail.com>
2020-09-30 17:48:00 -0500

    created questions in js

--- js/quiz.js
+++ js/quiz.js
@@ -2,7 +2,15 @@ var questions = [{
     question:"What color is the sky?",
     choices:["green","yellow","blue","purple"],
     answer:"blue"
-},  
+},  {
+    question:"What color is dirt?",
+    choices:["brown" , "red", "black", "green"],
+    answer:"black"
+},  {
+    question:"How many feet are in a mile?",
+    choices:["525000", "60", "5280", "2300"]
+    answer:"5280"
+}
 ]
 
 var startDiv = document.getElementById("start");
