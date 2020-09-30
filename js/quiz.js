f43e87125092d8cad39df152814369cf1d8d2608
Sam Fullerton <Samdfullerton@gmail.com>
2020-09-29 19:17:10 -0500

    initial file setup

---
+++ js/quiz.js
@@ -0,0 +1,12 @@
+var questions = [{
+    question:"What color is the sky?",
+    choices:["green","yellow","blue","purple"],
+    answer:"blue"
+},  
+]
+
+var startDiv = document.getElementById("start");
+var questionsDiv = document.getElementById("questions")
+var endDiv = document.getElementById("end")
+var questionIndex = 0
+function 
